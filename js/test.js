24fb7871776c41821f668592d114c336a533024f
Max-Hu <huzhiwise@gmail.com>
2016-02-23 12:19:06 +0800

    first draft

--- js/test.js
+++ js/test.js
@@ -1,8 +1,70 @@
-$('#my-button').bind('click', function() {
-    var input1 = $("#input1").val();
-    var input2 = $("#input2").val();
-    var input3 = $("#input3").val();
 
+var outputArray = new Array("Fizz","Buzz","Whizz");
+var outputHtml = "";
+window.onload = function(){
+  var btn = document.getElementById("my-button");
+  btn.onclick = function(){
+    var inputArray = new Array(parseInt($("#input1").val()),parseInt($("#input2").val()),parseInt($("#input3").val()));
+    if (isNumber(inputArray[0]) && isNumber(inputArray[1]) && isNumber(inputArray[2])) {
+      playGame(inputArray);
+    }else {
+      alert("输入数字错误！");
+    }
+  }
+}
 
+function playGame(inputArray) {
+  for (var i = 1; i < 101; i++) {
+    if(isRoleFive(i,inputArray[0])){
+      continue;
+    }else {
+      var result = "";
+      for (var j = 0; j < inputArray.length; j++) {
+        if (isDivisible(inputArray[j],i)) {
+          result += outputArray[j];
+        }
+      }
+      if (result == "") {
+        result = i;
+      }
+      createHtml(i + "==>" + result);
+    }
+  }
+  output();
+}
 
+function output() {
+  $("#output").html(outputHtml);
+}
+
+function isNumber(object){
+  if (_.isNumber(object) && object.toString().length ==1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function createHtml(output) {
+  var html = "<div>" + output + "</div>"
+  outputHtml += html;
+}
+
+function isDivisible(input,object) {
+  if (object%input == 0) {
+    return true;
+  }else {
+    return false;
+  }
+}
+
+function isRoleFive(number,input1){
+  if (number.toString().indexOf(input1)!=-1) {
+    var result = number + "==>" + outputArray[0];
+     createHtml(result);
+     return true;
+  }else {
+    return false;
+  }
 }
